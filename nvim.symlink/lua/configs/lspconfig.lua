ea049cd7b16805cbcc2a29e380335766321a86ae
yn5 <ynsnetmail@gmail.com>
2025-01-03 23:23:21 +0100

    add ts and eslint language servers to nvchad

--- nvim.symlink/lua/configs/lspconfig.lua
+++ nvim.symlink/lua/configs/lspconfig.lua
@@ -3,11 +3,9 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "ts_ls", "eslint" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
@@ -16,9 +14,3 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
